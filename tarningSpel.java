c8b92df6f0c8f3d2867e5a1e278425f552a61a2e
Filip <isberget23@gmail.com>
2025-10-01 11:10:03 +0200

    modified:   tarningSpel.java

--- tarningSpel.java
+++ tarningSpel.java
@@ -3,10 +3,10 @@ import java.util.*;
 
 public class tarningSpel {
     public static void main(String[] args) {
-        System.out.println("Välkommen till tärningsspelet! \nHär går leken ut på at ska du gissa summan av tärningnarna.");
+        System.out.println("Välkommen till tärningsspelet! \nHär går leken ut på at ska du gissa summan av tärningnarna. \nGissa vad den totala summan blir, du får 3 försök.");
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
-        int gissning = scanner.nextInt();
+        
         
         //Slår en tärning (1-6)
         int dice1 = rand.nextInt(6) + 1;
@@ -14,18 +14,26 @@ public class tarningSpel {
         int sum = dice1 + dice2;
 
         for(int i = 1; i <= 3; i++){
-        
+
+        int gissning = scanner.nextInt();
+
             if (gissning < 1 || gissning > 12 ){
             
             System.out.println("Fel värde! Bara tal mellan 1 och 12");
 
         }else if (gissning == sum){
             System.out.println("Du gissade rätt! Grattis!\n:)");
+            break;
 
         }
         else{
             System.out.println("FEL! Försök igen");
+            
         }
+        if (i == 3){
+            System.out.println("Du hadde 3 fel, du förlorade!");
+        }
+        
             
 
         }
